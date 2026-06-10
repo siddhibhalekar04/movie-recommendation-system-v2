18345ce84bbf5ef3011d2ec74d0c7f650fc69af2
unknown <siddhidbhalekar@gmail.com>
2026-06-10 13:05:51 +0530

    Movie recommendation system first working version

--- app.py
+++ app.py
@@ -1,69 +1,35 @@
 import pickle
 import streamlit as st
-import requests
 
-# ----------------- FETCH POSTER -----------------
-def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+st.title("🎬 Movie Recommendation System")
 
-
-# ----------------- RECOMMEND FUNCTION -----------------
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-
-    recommended_movie_names = []
-    recommended_movie_posters = []
-
-    for i in distances[1:6]:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_posters.append(fetch_poster(movie_id))
-        recommended_movie_names.append(movies.iloc[i[0]].title)
-
-    return recommended_movie_names, recommended_movie_posters
-
-
-# ----------------- LOAD FILES -----------------
 movies = pickle.load(open('movies.pkl', 'rb'))
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+st.write("Movies loaded ✔")
+
+# show dropdown safely
 movie_list = movies['title'].values
+selected_movie = st.selectbox("Select a movie", movie_list)
 
 
-# ----------------- UI -----------------
-st.title('🎬 Movie Recommendation System')
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
 
-selected_movie = st.selectbox(
-    "Type or select a movie",
-    movie_list
-)
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
 
-if st.button('Show Recommendation'):
-    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
+    recommended = []
 
-    col1, col2, col3, col4, col5 = st.columns(5)
+    for i in distances[1:6]:
+        recommended.append(movies.iloc[i[0]].title)
 
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
+    return recommended
 
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
+if st.button("Show Recommendation"):
+    results = recommend(selected_movie)
 
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
+    st.subheader("Recommended Movies:")
 
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+    for i in results:
+        st.write(i)
